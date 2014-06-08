699fdd42eb41287b17c24b9931f846eeefd45a9d
Heidi Petersen <thesquirrelfish@gmail.com>
2014-06-08 08:32:48 +0700

    moved gmaps.js into correct file, still not using it, added coord display

---
+++ app/assets/javascripts/gmaps.js
@@ -0,0 +1,11 @@
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCP5NViYBg_el5x_7G8GaKgkxihZig_wvo"></script>
+    <script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(12.0479159,102.3234816),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      }  
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
