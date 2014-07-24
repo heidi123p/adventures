1ee7881316adc0761875bc93fd9fca4d620efcc4
Heidi Petersen <thesquirrelfish@gmail.com>
2014-07-24 20:07:10 +0700

    map shows up, markers still don't - json is there
    ok, have locations.json passing all location data, but I still need to
    figure out how to pass that json hash into js maps
    
    The location of the setMarker loop is what causes the gap between map
    and table

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,11 @@ class LocationsController < ApplicationController
   # GET /locations.json
   def index
     @locations = Location.all
+    respond_to do |format|
+      format.html # index.hrml.erb
+      format.xml { render xml: @locations}
+      format.json { render json: @locations}
+    end
   end
 
   # GET /locations/1
--- app/views/locations/index.json.jbuilder
+++ app/views/locations/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@locations) do |location|
-  json.extract! location, :id, :address, :adventure_id
+  json.extract! location, :id, :address, :adventure_id, :latitude, :longitude
   json.url location_url(location, format: :json)
 end
