f486ad939b9a9419057525a3a7620e0430da54a4
Heidi Petersen <thesquirrelfish@gmail.com>
2015-04-11 12:24:44 -0500

    added password, emptied user yaml, had issue with installing bcrypt
    i think i need to upgrade rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411163506) do
+ActiveRecord::Schema.define(version: 20150411171716) do
 
   create_table "adventures", force: true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150411163506) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
