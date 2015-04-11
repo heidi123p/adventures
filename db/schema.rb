f4e750c42c0b015585207d761a7d552aa4ba6b1c
Heidi Petersen <thesquirrelfish@gmail.com>
2015-04-11 11:40:37 -0500

    user without a password
    do I want a password for my user?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607125303) do
+ActiveRecord::Schema.define(version: 20150411163506) do
 
   create_table "adventures", force: true do |t|
     t.string   "name"
